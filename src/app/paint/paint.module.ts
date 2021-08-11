f2d6cd0289db3e4c03932106019af54d6ddc2dab
noubar <noubar.kaloust@gmail.com>
2021-08-11 16:02:46 +0400

    reusable  projects component

--- src/app/paint/paint.module.ts
+++ src/app/paint/paint.module.ts
@@ -8,6 +8,7 @@ import { CircleComponent } from './circle/circle.component';
 import { ProjectComponent } from './project/project.component';
 import {FormsModule} from "@angular/forms";
 import { HeaderComponent } from './header/header.component';
+import { SavedComponent } from './saved/saved.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
     CirclesComponent,
     CircleComponent,
     ProjectComponent,
-    HeaderComponent
+    HeaderComponent,
+    SavedComponent
   ],
   imports: [
     CommonModule,
